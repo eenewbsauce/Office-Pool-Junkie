7c16ae22c0f25572f8b502d8163aab5b77b7a78c
eenewbsauce <ryan.m.tracey@gmail.com>
2018-02-16 09:52:48 -0500

    refactor(scraping): update for new site design. auth not working though

--- scraping/auth.js
+++ scraping/auth.js
@@ -1,34 +1,41 @@
 const loginEndpoint = 'https://www.officepooljunkie.com/login.php';
-const email         = process.env.OFFICE_POOL_JUNKIE_EMAIL;
-const password      = process.env.OFFICE_POOL_JUNKIE_PASSWORD;
-const request       = require('request');
+const email = process.env.OFFICE_POOL_JUNKIE_EMAIL;
+const password = process.env.OFFICE_POOL_JUNKIE_PASSWORD;
+const request = require('request');
 
-const cookieJar     = require('./cookiejar');
+const cookieJar = require('./cookiejar');
 
 class Auth {
-  login() {
-      console.log('logging in');
-      return new Promise((resolve, reject) => {
-      request({
-          url: loginEndpoint,
-          method: 'POST',
-          form: {
-              Email: email,
-              Password: password
-          }
-      }, (err, res, body) => {
-        if (err) {
-          return reject(err);
-        }
+    login() {
+        console.log('logging in');
+        return new Promise((resolve, reject) => {
+            cookieJar.setCookie('PHPSESSID=kculg3ick5giqmc04ge423mph0');
+            return resolve({message: 'logged in!'});
 
-        cookieJar.setCookie(res.headers['set-cookie'][0]);
+            request({
+                url: loginEndpoint,
+                method: 'POST',
+                form: {
+                    email,
+                    password
+                }
+            }, (err, res, body) => {
+                if (err) {
+                    return reject(err);
+                }
 
-        resolve({message: 'logged in!'});
-      });
-    });
-  };
+                let cookie = res.headers['set-cookie'][0].split(';')[0];
+
+                console.log(cookie)
+
+                cookieJar.setCookie(cookie);
+
+                resolve({message: 'logged in!'});
+            });
+        });
+    };
 }
 
-module.exports = function() {
-  return new Auth();
-}
+module.exports = function () {
+    return new Auth();
+};
--- scraping/pages.js
+++ scraping/pages.js
@@ -1,154 +1,159 @@
-const baseUrl           = 'https://www.officepooljunkie.com';
-const poolsEndpoint     = `${baseUrl}/mypools.php`;
-const cheerio           = require('cheerio');
-const request           = require('request');
-const R                 = require('ramda');
-const querystring       = require('querystring');
+const baseUrl = 'https://www.officepooljunkie.com';
+const poolsEndpoint = `${baseUrl}/my-pools.php`;
+const cheerio = require('cheerio');
+const request = require('request');
+const R = require('ramda');
+const querystring = require('querystring');
 
-const teams             = require('../compare').teams;
-const cookieJar         = require('./cookiejar');
-const userAgent         = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36';
-const options           = require('../options').parse();
+const teams = require('../compare').teams;
+const cookieJar = require('./cookiejar');
+const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36';
+const options = require('../options').parse();
 
 class Pages {
-  setPoolId(pool) {
-    this.poolId = pool.link.split('=')[1];
-  }
+    setPoolId(pool) {
+        this.poolId = pool.link.split('=')[1];
+    }
 
-  getPoolId() {
-    return this.poolId;
-  }
+    getPoolId() {
+        return this.poolId;
+    }
 
-  listRead() {
-    console.log('getting pools');
-    let self = this;
+    listRead() {
+        console.log('getting pools');
+        let self = this;
 
-    return new Promise((resolve, reject) => {
-      request({
-        url: poolsEndpoint,
-        method: 'GET',
-        jar: cookieJar.getCookie()
-      }, (err, res, body) => {
-        if (err) {
-          return reject(err);
-        }
+        return new Promise((resolve, reject) => {
+            request({
+                url: poolsEndpoint,
+                method: 'GET',
+                jar: cookieJar.getCookie()
+            }, (err, res, body) => {
+                if (err) {
+                    return reject(err);
+                }
 
-        let $ = cheerio.load(body);
-        let rows = $('.create tr');
-        let pools = [];
+                let $ = cheerio.load(body);
+                let rows = $('.container .card');
+                let pools = [];
 
-        rows.each((i, el) => {
-          let cells = cheerio(el).find('td');
+                rows.each((i, el) => {
+                    let weekParts = $(el).find('.card-body .col-md-auto.text-nowrap p a').eq(0).text().split(':');
+                    let number = weekParts[0].split(' ')[1];
+                    let status = weekParts[1].trim().toLowerCase();
+                    let complete = status === 'pool over' || status === 'no games';
 
-          pools.push({
-              link: cells.eq(0).find('a.tooltip').attr('href'),
-              week: parseInt(cells.eq(1).text().split(':')[0].split(' ')[1]) || undefined,
-              isComplete: cells.eq(1).text().indexOf('Pool Over') !== -1 || cells.eq(1).text().indexOf('No Games') !== -1
-          });
+                    pools.push({
+                        link: $(el).find('h4.card-title a').attr('href'),
+                        week: number,
+                        isComplete: complete
+                    });
+                });
+
+
+                let cleanPools = R.compose(
+                    R.sortWith([R.ascend(R.prop('week'))]),
+                    R.filter(p => {
+                        return !p.isComplete;
+                    })
+                )(pools);
+
+                self.setPoolId(cleanPools[0]);
+
+
+                resolve({
+                    link: cleanPools[0].link,
+                    week: cleanPools[0].week
+                });
+            });
         });
+    }
 
-        let cleanPools = R.compose(
-            R.sortWith([R.ascend(R.prop('week'))]),
-            R.filter(p => {
-              return !p.isComplete && (!R.isNil(p.week) && typeof p.week === 'number');
-            })
-        )(pools);
+    poolRead(data) {
+        console.log('go to pool page');
 
-        self.setPoolId(cleanPools[0]);
+        return new Promise((resolve, reject) => {
+            request({
+                url: `${baseUrl}/${data.link}`,
+                method: 'GET',
+                jar: cookieJar.getCookie()
+            }, (err, res, body) => {
+                if (err) {
+                    return reject(err);
+                }
 
-        resolve({
-          link: cleanPools[0].link,
-          week: cleanPools[0].week
+                let $ = cheerio.load(body);
+                let rows = $('.table-responsive tbody tr');
+                let matchups = [];
+
+                rows.each((i, el) => {
+                    let cells = cheerio(el).find('td');
+                    matchups.push({
+                        away: {
+                            name: cells.eq(1).find('.col-6.text-nowrap').text().split('(')[0],
+                            abbv: cells.eq(1).attr('onclick').split('\'')[5]
+                        },
+                        home: {
+                            name: cells.eq(2).find('.col-6.text-nowrap').text().split('(')[0],
+                            abbv: cells.eq(2).attr('onclick').split('\'')[5]
+                        },
+                        game: {
+                            name: cells.eq(0).find('input').attr('name'),
+                            value: cells.eq(0).find('input').attr('value')
+                        }
+                    });
+                });
+
+                let cleanMatchups = R.filter(m => {
+                    return !R.isEmpty(m.away.name) && !R.isEmpty(m.home.name);
+                }, matchups);
+
+                resolve({
+                    matchups: cleanMatchups,
+                    week: data.week,
+                    h: $('input[name="h"]').attr('value')
+                });
+            });
         });
-      });
-    });
-  }
+    }
 
-  poolRead(data) {
-    console.log('go to pool page');
+    poolWrite(data, stats, algorithm, submitResults) {
+        return new Promise((resolve, reject) => {
+            let selectionData;
 
-    return new Promise((resolve, reject) => {
-        request({
-            url: `${baseUrl}/${data.link}`,
-            method: 'GET',
-            jar: cookieJar.getCookie()
-        }, (err, res, body) => {
-            if (err) {
-                return reject(err);
+            if (options.shouldSaveSelections) {
+                console.log(`making selections with algorithm: ${algorithm}`);
+                selectionData = teams.create(stats, algorithm).getSelections(data, this.getPoolId());
+            } else {
+                resolve();
             }
 
-            let $ = cheerio.load(body);
-            let rows = $('.sheet tr');
-            let matchups = [];
+            if (!submitResults) {
+                return resolve(selectionData);
+            }
 
-            rows.each((i, el) => {
-              let cells = cheerio(el).find('td');
-              matchups.push({
-                away: {
-                  name: cells.eq(3).text(),
-                  abbv: cells.eq(2).find('input').attr('value')
+            request({
+                url: `${baseUrl}/picks-pickem.php`,
+                method: 'POST',
+                headers: {
+                    'Connection': 'keep-alive',
+                    'User-Agent': userAgent,
+                    'Cache-Control': 'no-cache',
                 },
-                home: {
-                  name: cells.eq(6).text(),
-                  abbv: cells.eq(5).find('input').attr('value')
-                },
-                game: {
-                  name: cells.eq(0).find('input').attr('name'),
-                  value: cells.eq(0).find('input').attr('value')
+                jar: cookieJar.getCookie(),
+                qs: {Pool: this.poolId},
+                form: selectionData.selections
+            }, (err, res, body) => {
+                if (err) {
+                    return reject(err);
                 }
-              });
-            });
 
-            let cleanMatchups = R.filter(m => {
-              return !R.isEmpty(m.away.name) && !R.isEmpty(m.home.name);
-            }, matchups);
-
-            resolve({
-              matchups: cleanMatchups,
-              week: data.week,
-              h: $('input[name="h"]').attr('value')
+                resolve(selectionData);
             });
         });
-    });
-  }
-
-  poolWrite(data, stats, algorithm, submitResults) {
-      return new Promise((resolve, reject) => {
-          let selectionData;
-
-          if (options.shouldSaveSelections) {
-            console.log(`making selections with algorithm: ${algorithm}`);
-            selectionData = teams.create(stats, algorithm).getSelections(data, this.getPoolId());
-          } else {
-            resolve();
-          }
-
-          if (!submitResults) {
-              return resolve(selectionData);
-          }
-
-          request({
-              url: `${baseUrl}/picks_pickem.php`,
-              method: 'POST',
-              headers: {
-                  'Connection': 'keep-alive',
-                  'User-Agent': userAgent,
-                  'Cache-Control': 'no-cache',
-              },
-              jar: cookieJar.getCookie(),
-              qs: {Pool: this.poolId},
-              form: selectionData.selections
-          }, (err, res, body) => {
-              if (err) {
-                  return reject(err);
-              }
-
-              resolve(selectionData);
-          });
-      });
-  }
+    }
 }
 
-module.exports = function() {
-  return new Pages();
+module.exports = function () {
+    return new Pages();
 };
